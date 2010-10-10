22ea1130045a564ee419327489e3739a2fb84e4a
Shane Graber <sgraber@gmail.com>
2010-10-09 23:07:09 -0400

    fixed problem where image shifted to the right if the parent didn't have an explicit width set on it or the width that was set on it was different than the item being rendered

--- aaolm/portlet/floating/skins/floating/jquery.fixedscroll.js
+++ aaolm/portlet/floating/skins/floating/jquery.fixedscroll.js
@@ -11,74 +11,79 @@
 // This does not work in IE6.
 //
 // Usage:
-// Best to be called from the window load function so as to take into 
-// account any images that are loaded in the document.  Below is an example:
 //
-// $(window).load(function(){
-// 		$("#element").fixedscroll({
-//				padding: 20,
-//				boundary: '#element2'
-//		});
-//	});
+// $(document).ready(function(){
+// 	$("#element").fixedscroll({padding: 20,	boundary: '#element2'});
+// });
 //
-(function($){
- 	$.fn.extend({ 
- 		
- 		fixedscroll: function(options) {
- 		    
-			var defaults = { 
-			    padding: 0,
-           		boundary: null
-			};
-			
-			var options = $.extend(defaults, options);
 
-    		return this.each(function() {
-				var o = options;
-				var $this = $(this);				
-				  
-				var msie6 = $.browser == 'msie' && $.browser.version < 7;
+(function($){
+    $.fn.extend({ 
+        
+        fixedscroll: function(options) {
+            
+            var defaults = { 
+                padding: 0,
+                boundary: null
+            };
+            
+            var options = $.extend(defaults, options);
+
+            return this.each(function() {
+                var o = options;
+                var $this = $(this);
+
+                // explicitly set the width of the parent container otherwise 
+                // image positioning will shift left/right
+                // make sure that the DOM object you've targeted with $(this) has 
+                // an explicit width set on it otherwise this will NOT WORK
+                var left = $this.offset().left;
+                var parentWidth = $this.parent().width();
+                $this.parent().css({'width':parentWidth+'px'});
+                  
+                var msie6 = $.browser == 'msie' && $.browser.version < 7;
 
                 if (!msie6) {
-					// this is our position from the top of the screen
-					// var top = $this.offset().top - parseFloat($this.css('margin-top').replace(/auto/, 0)) - o.padding;   
-					var top = $this.offset().top - (parseFloat($('#comment').css('margin-top')) || 0) - o.padding;
-					// this is the height of the element we want to fix
-					var h = $this.outerHeight();  
-					
-					$(window).scroll(function (event) {
-						var y = $(this).scrollTop();    // this is where we are vertically on the page
-						
-						// this is the element we want the item to stop before
-						// moved into the loop in case we're lazy loading images
-						var bottom = 0;
-						if (!(o.boundary==null)) {
-							bottom = $(o.boundary).offset().top - h - o.padding; 
-						}
-						
-						var b = bottom - y;             // this is how close to the bottom we are
-                        
-						// check to see if we've scrolled and the element is touching the window top
-                        if (y >= top) {
-							// if we've scrolled and are touching, fix the element in place
-							$this.css({'position':'fixed', 'top':o.padding+'px'});
-							
-							// now let's see how close to the bottom we are
-							if (bottom && b <= 0) {     
-							    // if we're at the bottom, freeze the ad in place
-							    $this.css({'position':'absolute', 'top':bottom+'px'});
-    			            } else {  
-    			                // if we're not at the bottom (anymore?), remove the style attr
-    			                $this.css({'position':'fixed', 'top':o.padding+'px'});
-    			            }
-                        } else {
-							// otherwise remove it
-							$this.css({'position':'relative', 'top':'auto'}); 
+                    // this is our position from the top of the screen
+                    // make sure that the DOM element exists
+                    var top = $this.offset().top - (parseFloat($('#portal-footer').css('margin-top')) || 0) - o.padding;
+
+                    // this is the height of the element we want to fix
+                    var h = $this.outerHeight();  
+                    
+                    $(window).scroll(function (event) {
+                        var y = $(this).scrollTop();    // this is where we are vertically on the page
+                    
+                        // this is the element we want the item to stop before
+                        // moved into the loop in case we're lazy loading images
+                        var bottom = 0;
+                        if (!(o.boundary==null)) {
+                            bottom = $(o.boundary).offset().top - h - o.padding; 
                         }
-					});
+
+                        var b = bottom - y;             // this is how close to the bottom we are
+
+                        // check to see if we've scrolled and the element is touching the window top
+                        if (y >= top) {
+                            // if we've scrolled and are touching, fix the element in place
+                            $this.css({'position':'fixed', 'top':o.padding+'px', 'left':left+'px'});
+                            
+                            // now let's see how close to the bottom we are
+                            if (bottom && b <= 0) {     
+                                // if we're at the bottom, freeze the ad in place
+                                $this.css({'position':'absolute', 'top':bottom+'px'});
+                            } else {  
+                                // if we're not at the bottom (anymore?), remove the style attr
+                                $this.css({'position':'fixed', 'top':o.padding+'px'});
+                            }
+                        } else {
+                            // otherwise remove it
+                            $this.css({'position':'static', 'top':'auto'}); 
+                        }
+                    });
                 }
-    		});
-    	}
-	});
-	
+            });
+        }
+    });
+    
 })(jQuery);
